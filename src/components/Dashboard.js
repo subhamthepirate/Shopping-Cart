2351305e46f5fdf1f49e4532698eed861049990d
subhamthepirate <patrasubham82@gmail.com>
2022-05-29 18:28:30 +0530

    first commit

---
+++ src/components/Dashboard.js
@@ -0,0 +1,74 @@
+import React, { useState,useEffect } from "react"
+import { Card, Button, Alert } from "react-bootstrap"
+import { useAuth } from "../contexts/AuthContext"
+import { Link, useHistory } from "react-router-dom"
+import axios from 'axios';
+import { InfinitySpin } from  'react-loader-spinner'
+
+export default function Dashboard(props) {
+  const [states, setStates] = useState([]);
+  useEffect(() => {
+    axios
+    .get("https://fakestoreapi.com/products/categories")
+    .then((data) => {
+    setStates(data.data);
+    console.log(states);
+    })
+    .catch((error) => {
+    console.log(error);
+    })
+ },[]);
+ console.log(states)
+  const [error, setError] = useState("")
+  const { currentUser, logout } = useAuth()
+  const history = useHistory()
+
+  async function handleLogout() {
+    setError("")
+
+    try {
+      await logout()
+      history.push("/login")
+    } catch {
+      setError("Failed to log out")
+    }
+  }
+  //console.log(states)
+  const listItems = states.map((category,index) =>
+    <Card>
+      <Card.Body>
+        <Link to="/products"><h2 className="text-center mb-4">{category}</h2></Link>
+      </Card.Body>
+    </Card>
+  );
+
+  return (
+    <>
+    {states.length?
+    <p>
+    {listItems}</p>
+    :
+      <p><InfinitySpin
+      height="100"
+    width="100"
+    color='grey'
+    ariaLabel='loading'
+    /></p>
+      }
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Profile</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <strong>Email:</strong> {currentUser.email}
+          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
+            Update Profile
+          </Link>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        <Button variant="link" onClick={handleLogout}>
+          Log Out
+        </Button>
+      </div>
+    </>
+  )}
---
+++ src/components/Products.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Navbar,Container,Row, Card, Button, CardGroup } from "react-bootstrap"
+import {useEffect,useState} from 'react'
+import { Link } from "react-router-dom"
+import axios from 'axios'
+import { Audio } from  'react-loader-spinner'
+import { FaShoppingCart,FaCartPlus,FaStar } from 'react-icons/fa';
+
+function Products() {
+    const [states, setStates] = useState([]);
+    useEffect(() => {
+        axios
+        .get("https://fakestoreapi.com/products/")
+        .then((data) => {
+        setStates(data.data);
+        console.log(states);
+        })
+        .catch((error) => {
+        console.log(error);
+        })
+     },[]);
+console.log(states)
+  return (
+    <div>
+        <Navbar expand="lg" variant="light" bg="light" sticky="top" >
+        <Container>
+          <Navbar.Brand href="#">Shopping Kart</Navbar.Brand>
+          <span>
+          <Link to="/cart"><FaShoppingCart /></Link>
+          </span>
+        </Container>
+        </Navbar>
+        {states.length?
+        <Row xs={2} md={2} className="g-6">
+            { states.map((category,index) =>
+              
+            <CardGroup>
+                <Card style={{ width: '20rem'}}>
+                <Card.Img variant="top" src={category.image} width="20px" length="20px"/>
+                <Card.Body>
+                    {console.log(category)}
+                <Card.Title>{category.title}</Card.Title>
+                <Card.Text>{category.description}</Card.Text>
+                <Card.Text><h5>Ratings </h5> {category.rating.rate} <FaStar/></Card.Text>
+                <Card.Text><h5>Price:</h5>{category.price}</Card.Text>
+                <Button variant="primary">Add to cart <FaCartPlus/></Button>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">{category.category}</small>
+                </Card.Footer>
+                </Card>
+            </CardGroup>
+          )}
+          </Row>
+        :
+            <Audio
+            height="100"
+            width="100"
+            color='grey'
+            ariaLabel='loading' />
+      }
+    </div>
+  )
+}
+
+export default Products
